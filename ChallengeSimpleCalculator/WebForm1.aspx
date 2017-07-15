c8dc03c14915eb9431f0ce584c4de75debf38ca5
Arthurburkey <Arthurburkey@email.com>
2017-07-15 15:22:53 -0700

    C# Simple Calculator Drill
    Calculator created through Visual Studio.

---
+++ ChallengeSimpleCalculator/WebForm1.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ChallengeSimpleCalculator.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="font-size: medium">
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <h1>Simple Calculator</h1>
+        <p>
+            First Value:<asp:TextBox ID="firstTextBox" runat="server" OnTextChanged="TextBox2_TextChanged"></asp:TextBox>
+        </p>
+        <p>
+            Second Value:<asp:TextBox ID="secondTextBox" runat="server" OnTextChanged="TextBox3_TextChanged"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="additionButton" runat="server" Text="+" OnClick="additionButton_Click" />
+&nbsp;<asp:Button ID="subtractionButton" runat="server" Text="-" OnClick="subtractionButton_Click" />
+&nbsp;<asp:Button ID="multiplicationButton" runat="server" Text="*" OnClick="multiplicationButton_Click" style="height: 26px" />
+&nbsp;<asp:Button ID="divisionButton" runat="server" Text="/" OnClick="divisionButton_Click" />
+        </p>
+        <p id="resultLabel">
+            <asp:Label ID="resultLabel" runat="server" style="font-size: larger; font-weight: 700"></asp:Label>
+        </p>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ ChallengeSimpleCalculator/WebForm1.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeSimpleCalculator
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void additionButton_Click(object sender, EventArgs e)
+        {
+            int first = int.Parse(firstTextBox.Text);
+            int second = int.Parse(secondTextBox.Text);
+            int result = first + second;
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void subtractionButton_Click(object sender, EventArgs e)
+        {
+            int first = int.Parse(firstTextBox.Text);
+            int second = int.Parse(secondTextBox.Text);
+            int result = first - second;
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void multiplicationButton_Click(object sender, EventArgs e)
+        {
+            int first = int.Parse(firstTextBox.Text);
+            int second = int.Parse(secondTextBox.Text);
+            int result = first * second;
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void divisionButton_Click(object sender, EventArgs e)
+        {
+            double first = double.Parse(firstTextBox.Text);
+            double second = double.Parse(secondTextBox.Text);
+            double result = first / second;
+            resultLabel.Text = result.ToString();
+        }
+    }
+}
